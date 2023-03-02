eab49619f7d9ad5dc4ea382149c5dfd50a5f2310
javim <javim@DESKTOP-RFHBISD>
2023-03-02 20:49:39 +0100

    ESTE ES UN MENSAJE DE PRUEBA PARA EL PUNTO 4

---
+++ src/Proyecto/ParImpar.java
@@ -0,0 +1,26 @@
+package Proyecto;
+
+import java.util.Scanner;
+
+public class ParImpar {
+
+	public static void main(String[] args) {
+
+		Scanner tecla = new Scanner(System.in);
+		
+		int num1;
+		
+		do {
+			System.out.println("Escribe un numero POSITIVO: ");
+			num1=tecla.nextInt();
+		}while (num1<0);
+		
+		if(num1%2==0) {
+			System.out.println("El numero es par");
+		}else {
+			System.out.println("El numero es impar");
+		}
+		tecla.close();
+	}
+
+}
